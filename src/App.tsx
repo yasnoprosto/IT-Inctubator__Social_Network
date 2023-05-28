c678f834e4edcf1b39d337b2a0dbe41a640a29a7
Denis Pochinskiy <ircxenon1@gmail.com>
2023-05-28 09:18:50 +0500

    export default

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Header from "./Header";
+import Technologies from "./Technologies";
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Technologies />
+      </div>
+  );
+};
+
+export default App;
---
+++ src/Header.tsx
@@ -0,0 +1,12 @@
+
+const Header = () => {
+  return (
+    <div>
+      <a href="#">Home</a>
+      <a href="#">News</a>
+      <a href="#">Feed</a>
+    </div>
+  );
+};
+
+export default Header;
